b7897dfdf6ce0f8d7119294c517a717420817fef
Anand <101801007@smail.iitpkd.ac.in>
2022-10-26 13:04:59 +0530

    bug fix2

--- pages/index.js
+++ pages/index.js
@@ -48,9 +48,9 @@ export default function Home() {
           width={300} height={100} />
           <div className='flex w-full mt-5 hover:shadow-lg focus-within:shadow-lg max-w-md
           rounded-full border border-gray-200 px-5 py-3 items-center sm:max-w-xl lg:max-w-2xl'>
-            <SearchIcon className='h-5 mr-3 text-gray-500' />
+            <SearchIcon className='h-5 mr-3 text-gray-500 min-h-5' />
             <input ref={searchInputRef} type='text' className='focus:outline-none flex-1' />
-            <MicrophoneIcon className='h-5 cursor-pointer' />
+            <MicrophoneIcon className='h-5 cursor-pointer min-h-5' />
           </div>
           <div className='flex flex-col w-1/2 space-y-2 justify-center mt-8 
           sm:space-y-0 sm:flex-row sm:space-x-4'>
