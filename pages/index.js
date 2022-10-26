3a004ab9c81c3c64a72957b31e8c19d20adee0fe
Anand <101801007@smail.iitpkd.ac.in>
2022-10-26 13:30:41 +0530

    bug fix8

--- pages/index.js
+++ pages/index.js
@@ -47,7 +47,7 @@ export default function Home() {
           <Image src='https://www.google.com/images/branding/googlelogo/2x/googlelogo_color_272x92dp.png' 
           width={300} height={100} />
           <div className='flex w-full mt-5 hover:shadow-lg focus-within:shadow-lg max-w-md
-          rounded-full border border-gray-200 px-5 py-3 items-center sm:max-w-xl lg:max-w-2xl'>
+          rounded-full border border-gray-200 px-5 py-3 items-center sm:max-w-xl lg:max-w-2xl justify-center'>
             <SearchIcon className='h-5 mr-3 text-gray-500 shrink-0' />
             <input ref={searchInputRef} type='text' className='focus:outline-none flex-grow' />
             <MicrophoneIcon className='pr-3 h-5 cursor-pointer shrink-0' />
